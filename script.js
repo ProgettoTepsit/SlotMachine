699e015ceb1140f5611fd9380caae27e0293f808
simonepanfilii <simone.panfili@studenti.iispascal.it>
2023-02-27 09:52:20 +0100

    056

---
+++ script.js
@@ -0,0 +1,18 @@
+var reel1 = document.getElementById("reel1");
+var reel2 = document.getElementById("reel2");
+var reel3 = document.getElementById("reel3");
+
+var images = ["carota.jfif", "cipo.jfif", "melanzana.jpg"];
+
+function spin() {
+	var random1 = Math.floor(Math.random() * images.length);
+	var random2 = Math.floor(Math.random() * images.length);
+	var random3 = Math.floor(Math.random() * images.length);
+	reel1.src = images[random1];
+	reel2.src = images[random2];
+	reel3.src = images[random3];
+	
+	if (random1 === random2 && random2 === random3) {
+		alert("You won!");
+	}
+}
