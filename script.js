45042d4f02203b42d575a536c560665601d04eba
simonepanfilii <104305887+simonepanfilii@users.noreply.github.com>
2023-04-23 17:47:51 +0200

    oooooooooooo

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-var reel1 = document.getElementById("reel1");
-var reel2 = document.getElementById("reel2");
-var reel3 = document.getElementById("reel3");
+const reel1 = document.querySelector('.reel:nth-child(1)');
+const reel2 = document.querySelector('.reel:nth-child(2)');
+const reel3 = document.querySelector('.reel:nth-child(3)');
+const spinButton = document.querySelector('.spin-button');
 
-var images = ["carota.jfif", "cipo.jfif", "melanzana.jpg"];
+spinButton.addEventListener('click', () => {
+  spin(reel1);
+  spin(reel2);
+  spin(reel3);
+});
 
-function spin() {
-	var random1 = Math.floor(Math.random() * images.length);
-	var random2 = Math.floor(Math.random() * images.length);
-	var random3 = Math.floor(Math.random() * images.length);
-	reel1.src = images[random1];
-	reel2.src = images[random2]; 
-	reel3.src = images[random3];
-	
-	if (random1 === random2 && random2 === random3) {
-		alert("You won!");
-	}
-}
-
-function game(){
-	window.location.href = "4444444444.html";
+function spin(reel) {
+  const symbols = reel.querySelectorAll('.img');
+  const randomSymbolIndex = Math.floor(Math.random() * symbols.length);
+  const spinDistance = randomSymbolIndex * -100;
+  reel.style.transform = `translateY(${spinDistance}%)`;
+  reel.classList.add('spin');
+  reel.addEventListener('animationend', () => {
+    reel.classList.remove('spin');
+    reel.style.transform = 'translateY(0)';
+  }, {once: true});
 }
